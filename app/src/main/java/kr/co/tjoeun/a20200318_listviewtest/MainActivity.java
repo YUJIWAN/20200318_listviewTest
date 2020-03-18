e7de880439216dcb341685b73196ca0a323259dd
YUJIWAN <61855068+YUJIWAN@users.noreply.github.com>
2020-03-18 17:57:40 +0900

    LongClick이벤트를 삭제로 변경

--- app/src/main/java/kr/co/tjoeun/a20200318_listviewtest/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_listviewtest/MainActivity.java
@@ -59,8 +59,13 @@ public class MainActivity extends BaseActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
 //                꾹 누르고 있으면, 해당 방의 설명을 Toast로 출력
-                Room data = roomDatas.get(position);
-                Toast.makeText(mContext, data.getDescription(),Toast.LENGTH_LONG).show();
+//                Room data = roomDatas.get(position);
+//                Toast.makeText(mContext, data.getDescription(),Toast.LENGTH_LONG).show();
+
+//                꾹 누르면 해당 아이템을 목록에서 삭제
+                roomDatas.remove(position);
+//                어댑터에게 새로고침 시킴.
+                roomAdapter.notifyDataSetChanged();
 
                 return false; // true : 롱클릭만. false : 그냥클릭도 같이
             }
